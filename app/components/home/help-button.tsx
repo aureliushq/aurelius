a3f07c3d42acad0c245cd06796151290492e9915
Ilango <rajagopalilango@gmail.com>
2024-07-11 01:04:02 +0530

    Add more ui components and show e2e encrypted shield and tooltip

--- app/components/home/help-button.tsx
+++ app/components/home/help-button.tsx
@@ -4,16 +4,14 @@ import { HelpDialogProps } from '~/lib/types'
 
 const HelpButton = ({ setHelpOpen }: HelpDialogProps) => {
 	return (
-		<div className='absolute bottom-4 right-4'>
-			<Button
-				className='w-9 h-9'
-				onClick={() => setHelpOpen(true)}
-				size='icon'
-				variant='secondary'
-			>
-				<CircleHelpIcon className='w-4 h-4' />
-			</Button>
-		</div>
+		<Button
+			className='w-9 h-9'
+			onClick={() => setHelpOpen(true)}
+			size='icon'
+			variant='secondary'
+		>
+			<CircleHelpIcon className='w-4 h-4' />
+		</Button>
 	)
 }
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,11 +3,18 @@ import { Suspense, startTransition, useState } from 'react'
 import type { MetaFunction } from '@remix-run/node'
 
 import { useQuery } from '@evolu/react'
+import { ShieldCheckIcon } from 'lucide-react'
 import PreferencesDialog from '~/components/common/preferences-dialog'
 import HelpButton from '~/components/home/help-button'
 import HelpDialog from '~/components/home/help-dialog'
 import MainMenu from '~/components/home/main-menu'
 import SplashDialog from '~/components/home/splash-dialog'
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '~/components/ui/tooltip'
 import { settingsQuery } from '~/services/evolu/client'
 
 export const meta: MetaFunction = () => {
@@ -16,9 +23,10 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
 	const { rows } = useQuery(settingsQuery)
-	const settings = rows[0]
 	const [helpOpen, setHelpOpen] = useState(false)
 	const [preferencesOpen, setPreferencesOpen] = useState(false)
+
+	const settings = rows[0]
 
 	const handlePreferencesOpen = (state: boolean) => {
 		startTransition(() => {
@@ -40,7 +48,24 @@ export default function Index() {
 						settings={settings}
 					/>
 				</Suspense>
-				<HelpButton setHelpOpen={setHelpOpen} />
+				<div className='absolute bottom-4 right-4 flex items-center gap-4'>
+					<TooltipProvider>
+						<Tooltip>
+							<TooltipTrigger className='p-0' asChild>
+								<ShieldCheckIcon className='w-5 h-5 text-primary' />
+							</TooltipTrigger>
+							<TooltipContent>
+								<p className='text-sm text-center'>
+									All your data is stored in your device. Sync
+									is end-to-end encrypted so <br /> Aurelius
+									servers can&apos;t read your data.
+								</p>
+							</TooltipContent>
+						</Tooltip>
+					</TooltipProvider>
+
+					<HelpButton setHelpOpen={setHelpOpen} />
+				</div>
 			</div>
 			<HelpDialog setHelpOpen={setHelpOpen} helpOpen={helpOpen} />
 			<Suspense>
