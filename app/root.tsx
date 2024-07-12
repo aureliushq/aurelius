261534c8a798b8ed24374fc9591e9d25471f0b4e
Ilango <rajagopalilango@gmail.com>
2024-07-13 00:37:41 +0530

    Add fonts links to root

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,12 @@ import stylesheet from '~/globals.css?url'
 import { evolu } from '~/services/evolu/client'
 
 export const links: LinksFunction = () => [
+	{ rel: 'preconnect', href: 'https://fonts.bunny.net' },
+	{
+		rel: 'stylesheet',
+		href: 'https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900|lato:400,700|libre-baskerville:400,700|merriweather:400,700|noto-serif:400,700|open-sans:400,700|pt-serif:400,700|roboto:400,700',
+		crossOrigin: 'anonymous',
+	},
 	{ rel: 'stylesheet', href: stylesheet },
 ]
 
@@ -28,7 +34,7 @@ export default function App() {
 				<Meta />
 				<Links />
 			</head>
-			<body className='w-screen h-screen'>
+			<body className='w-screen h-screen !p-0'>
 				<EvoluProvider value={evolu}>
 					<Outlet />
 					<Toaster />
