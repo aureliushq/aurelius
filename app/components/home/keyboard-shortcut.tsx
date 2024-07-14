037f7f64857c81ef56507846adf130f1ba494bae
Ilango <rajagopalilango@gmail.com>
2024-07-14 20:27:23 +0530

    Fix missing key prop warning

--- app/components/home/keyboard-shortcut.tsx
+++ app/components/home/keyboard-shortcut.tsx
@@ -1,19 +1,18 @@
+import { Fragment } from 'react'
+
 const KeyboardShortcut = ({ keys }: { keys: string }) => {
 	if (!keys) return null
 
 	return (
 		<p className='flex items-center gap-1 text-xs'>
 			{keys.split('+').map((key) => (
-				<>
+				<Fragment key={key}>
 					{key && (
-						<kbd
-							className='px-1 py-0.5 font-semibold text-foreground bg-card border border-border rounded text-xs shadow-md'
-							key={key}
-						>
+						<kbd className='px-1 py-0.5 font-semibold text-foreground bg-card border border-border rounded text-xs shadow-md'>
 							{key}
 						</kbd>
 					)}
-				</>
+				</Fragment>
 			))}
 		</p>
 	)
