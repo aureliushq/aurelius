32276e69c866c534891483879afac5d4f37f051f
Ilango <rajagopalilango@gmail.com>
2024-07-27 22:12:14 +0530

    Implement error boundary for root

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,20 @@
+import { ReactNode } from 'react'
+
 import { LinksFunction, LoaderFunction } from '@remix-run/node'
 import {
+	Link,
 	Links,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
 	useLoaderData,
+	useRouteError,
 } from '@remix-run/react'
 
 import { EvoluProvider } from '@evolu/react'
 import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from 'remix-themes'
+import { Button } from '~/components/ui/button'
 import { Toaster } from '~/components/ui/toaster'
 import { TooltipProvider } from '~/components/ui/tooltip'
 import stylesheet from '~/globals.css?url'
@@ -33,14 +38,14 @@ export const loader: LoaderFunction = async ({ request }) => {
 	}
 }
 
-function App() {
+const App = ({ children, title }: { children: ReactNode; title?: string }) => {
 	const data = useLoaderData<typeof loader>()
 	const [theme] = useTheme()
 
 	return (
 		<html className={theme ?? 'dark'} lang='en'>
 			<head>
-				<title>Aurelius</title>
+				{title ? <title>{title}</title> : <title>Aurelius</title>}
 				<meta charSet='utf-8' />
 				<meta
 					name='viewport'
@@ -51,7 +56,7 @@ function App() {
 				<Links />
 			</head>
 			<body className='w-screen h-screen !p-0'>
-				<Outlet />
+				{children}
 				<Toaster />
 				<ScrollRestoration />
 				<Scripts />
@@ -60,16 +65,60 @@ function App() {
 	)
 }
 
-export default function AppWithProviders() {
+export const ErrorBoundary = () => {
+	const data = useLoaderData<typeof loader>()
+	const error = useRouteError()
+
+	return (
+		<ThemeProvider specifiedTheme={data?.theme} themeAction='/action/theme'>
+			<App title='Oh no!'>
+				<div className='flex w-full h-full items-center justify-center'>
+					<div className='flex flex-col items-center gap-4'>
+						<img
+							className='w-96 h-96 invert'
+							src='/images/crashed-error.svg'
+						/>
+						<h1 className='text-4xl font-bold'>Oh no!</h1>
+						{
+							// @ts-expect-error: it's fine
+							error?.message ? (
+								<p className='text-lg text-gray-500'>
+									{
+										// @ts-expect-error: it's fine
+										error?.message
+									}
+								</p>
+							) : (
+								<p className='text-lg text-gray-500'>
+									Something unexpected happened! Please try
+									again later.
+								</p>
+							)
+						}
+						<Link to='/'>
+							<Button>Back to Home</Button>
+						</Link>
+					</div>
+				</div>
+			</App>
+		</ThemeProvider>
+	)
+}
+
+const AppWithProviders = () => {
 	const data = useLoaderData<typeof loader>()
 
 	return (
 		<ThemeProvider specifiedTheme={data?.theme} themeAction='/action/theme'>
 			<EvoluProvider value={evolu}>
 				<TooltipProvider>
-					<App />
+					<App>
+						<Outlet />
+					</App>
 				</TooltipProvider>
 			</EvoluProvider>
 		</ThemeProvider>
 	)
 }
+
+export default AppWithProviders
