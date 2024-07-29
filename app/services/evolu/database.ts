1305cf3b3e27d49d3db6ee82ca21658c57685726
Ilango <rajagopalilango@gmail.com>
2024-07-29 22:29:37 +0530

    Update evalu database creation - add the new tables and add new types

--- app/services/evolu/database.ts
+++ app/services/evolu/database.ts
@@ -1,22 +1,68 @@
 import { database } from '@evolu/common'
 import {
+	BookId,
+	Books,
+	BooksTable,
+	ChapterId,
+	Chapters,
+	ChaptersTable,
+	EssayId,
+	Essays,
+	EssaysTable,
+	HelpId,
+	HelpTable,
+	JournalId,
+	Journals,
+	JournalsTable,
+	PostId,
+	Posts,
+	PostsTable,
+	Settings,
+	SettingsId,
 	SettingsTable,
-	WritingEffortTable,
-	WritingSessionTable,
-	WritingTable,
-	_HelpTable,
+	WritingEffortId,
+	WritingEfforts,
+	WritingEffortsTable,
+	WritingSessionId,
+	WritingSessions,
+	WritingSessionsTable,
+	_Help,
 } from '~/services/evolu/schema'
 
 const FIELDS = {
-	_help: _HelpTable,
-	settings: SettingsTable,
-	writing: WritingTable,
-	writingEffort: WritingEffortTable,
-	writingSession: WritingSessionTable,
+	_help: _Help,
+	books: Books,
+	chapters: Chapters,
+	essays: Essays,
+	journals: Journals,
+	posts: Posts,
+	settings: Settings,
+	writingEfforts: WritingEfforts,
+	writingSessions: WritingSessions,
 }
 
 const Database = database(FIELDS)
 export type Database = typeof Database.Type
 export type TableName = keyof Database
+export type Table =
+	| BooksTable
+	| ChaptersTable
+	| EssaysTable
+	| JournalsTable
+	| PostsTable
+	| SettingsTable
+	| WritingEffortsTable
+	| WritingSessionsTable
+	| HelpTable
+export type Id =
+	| BookId
+	| ChapterId
+	| EssayId
+	| HelpId
+	| JournalId
+	| PostId
+	| SettingsId
+	| WritingEffortId
+	| WritingSessionId
 
 export { Database }
