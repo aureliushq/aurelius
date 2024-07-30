92a4bf2673cd9c983e6d4eecb01c37bf8213a9a3
Ilango <rajagopalilango@gmail.com>
2024-07-30 09:47:12 +0530

    Remove all unused hooks

--- app/lib/hooks/index.ts
+++ app/lib/hooks/index.ts
@@ -1,15 +1,4 @@
 import useAutoSave from './useAutoSave'
 import { useKeyboardShortcuts } from './useKeyboardShortcuts'
-import useSettingsQuery from './useSettingsQuery'
-import useWritingEffortQuery from './useWritingEffortQuery'
-import useWritingQuery from './useWritingQuery'
-import useWritingSessionQuery from './useWritingSessionQuery'
 
-export {
-	useAutoSave,
-	useKeyboardShortcuts,
-	useSettingsQuery,
-	useWritingQuery,
-	useWritingEffortQuery,
-	useWritingSessionQuery,
-}
+export { useAutoSave, useKeyboardShortcuts }
