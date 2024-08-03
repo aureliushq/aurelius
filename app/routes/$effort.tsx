6d4bb3b5784be575b729cbabc5cf02eb300f9541
Ilango <rajagopalilango@gmail.com>
2024-08-03 21:00:00 +0530

    Fix issue with help content not loading

--- app/routes/$effort.tsx
+++ app/routes/$effort.tsx
@@ -1,29 +1,8 @@
-import {
-	ClientLoaderFunctionArgs,
-	Outlet,
-	useLoaderData,
-} from '@remix-run/react'
+import { Outlet } from '@remix-run/react'
 
-import * as S from '@effect/schema/Schema'
-import invariant from 'tiny-invariant'
 import EffortLayout from '~/layouts/effort'
-import { arls } from '~/services/arls'
-import { NonEmptyString100 } from '~/services/evolu/schema'
-
-export const clientLoader = async ({ params }: ClientLoaderFunctionArgs) => {
-	invariant(params.effort, 'Writing Effort cannot be empty')
-
-	const effort = await arls.writingEfforts.findUnique({
-		slug: S.decodeSync(NonEmptyString100)(params.effort),
-	})
-	invariant(effort, 'Writing effort not found')
-
-	return { effort }
-}
 
 const EffortRoot = () => {
-	const { effort } = useLoaderData<typeof clientLoader>()
-
 	return (
 		<EffortLayout>
 			<Outlet />
