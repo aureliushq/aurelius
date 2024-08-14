3f1d8878cd44d1c80e9fccac42443a9716cf6b85
Ilango <rajagopalilango@gmail.com>
2024-08-14 11:16:16 +0530

    Move global shortcuts and their actions to provider

--- app/lib/providers/aurelius.tsx
+++ app/lib/providers/aurelius.tsx
@@ -1,20 +1,41 @@
-import { ReactNode, Suspense, createContext } from 'react'
+import { ReactNode, createContext, startTransition, useState } from 'react'
 import { Timer, useTimer } from 'react-use-precision-timer'
 
+import { useNavigate } from '@remix-run/react'
+
 import { useQuery } from '@evolu/react'
-import LoadingScreen from '~/components/common/loading-screen'
+import { useKeyboardShortcuts } from '~/lib/hooks'
+import { EditorShortcuts } from '~/lib/types'
 import { SettingsRow, settingsQuery } from '~/services/evolu/client'
 
 export type AureliusProviderData = {
+	helpOpen: boolean
+	setHelpOpen: (open: boolean) => void
+	mainMenuOpen: boolean
+	setMainMenuOpen: (open: boolean) => void
+	preferencesOpen: boolean
+	handlePreferencesOpen: (open: boolean) => void
+	splashOpen: boolean
+	handleSplashOpen: (open: boolean) => void
 	sessionTimer: Timer
 	settings: SettingsRow
+	triggerGlobalShortcut: (shortcutName: string) => void
 }
 
 export const AureliusContext = createContext<AureliusProviderData>({
+	helpOpen: false,
+	setHelpOpen: () => {},
+	mainMenuOpen: false,
+	setMainMenuOpen: () => {},
+	preferencesOpen: false,
+	handlePreferencesOpen: (open: boolean) => {},
+	splashOpen: false,
+	handleSplashOpen: (open: boolean) => {},
 	// @ts-expect-error: tradeoff between having this comment in multiple places or one place
 	sessionTimer: null,
 	// @ts-expect-error: tradeoff between having this comment in multiple places or one place
 	settings: null,
+	triggerGlobalShortcut: (shortcutName: string) => {},
 })
 
 type AureliusProviderProps = {
@@ -22,12 +43,59 @@ type AureliusProviderProps = {
 }
 
 const AureliusProvider = ({ children }: AureliusProviderProps) => {
-	const { rows: settings } = useQuery(settingsQuery)
+	const shortcuts = {
+		[EditorShortcuts.HELP]: () => setHelpOpen(!helpOpen),
+		[EditorShortcuts.MAIN_MENU]: () => setMainMenuOpen(!mainMenuOpen),
+		[EditorShortcuts.NEW_POST]: () => createNewPost(),
+		[EditorShortcuts.PREFERENCES]: () =>
+			handlePreferencesOpen(!preferencesOpen),
+		[EditorShortcuts.SPLASH_DIALOG]: () => handleSplashOpen(!splashOpen),
+	}
+
+	const { triggerShortcut: triggerGlobalShortcut } =
+		useKeyboardShortcuts(shortcuts)
+
+	const navigate = useNavigate()
+
+	const { rows } = useQuery(settingsQuery)
+	const settings = rows[0]
+
+	const [helpOpen, setHelpOpen] = useState(false)
+	const [mainMenuOpen, setMainMenuOpen] = useState(false)
+	const [preferencesOpen, setPreferencesOpen] = useState(false)
+	const [splashOpen, setSplashOpen] = useState(!!settings?.showSplashDialog)
+
 	const sessionTimer = useTimer()
 
+	const createNewPost = () => {
+		handleSplashOpen(false)
+		navigate('/editor/posts')
+	}
+
+	const handlePreferencesOpen = (open: boolean) => {
+		startTransition(() => {
+			setPreferencesOpen(open)
+		})
+	}
+
+	const handleSplashOpen = (open: boolean) => {
+		startTransition(() => {
+			setSplashOpen(open)
+		})
+	}
+
 	const data: AureliusProviderData = {
+		helpOpen,
+		setHelpOpen,
+		mainMenuOpen,
+		setMainMenuOpen,
+		preferencesOpen,
+		handlePreferencesOpen,
+		splashOpen,
+		handleSplashOpen,
 		sessionTimer,
-		settings: settings[0],
+		settings,
+		triggerGlobalShortcut,
 	}
 
 	return (
